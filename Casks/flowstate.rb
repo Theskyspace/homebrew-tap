85e7438dbabcc7820649931e91ffd790dd98e8bd
Akash Joshi <43512801+Theskyspace@users.noreply.github.com>
2026-07-20 01:31:28 +0530

    flowstate 0.3.0

--- Casks/flowstate.rb
+++ Casks/flowstate.rb
@@ -1,6 +1,6 @@
 cask "flowstate" do
-  version "0.2.0"
-  sha256 "4980707803f3e8c329b61d3497f203f4004ab21efb3359cfb747723c6ea8cf36"
+  version "0.3.0"
+  sha256 "863b76bc6344060f38404300f504d39b3ea4a94c5a43accb9c3799d64f91b1b0"
 
   url "https://github.com/Theskyspace/flowstate-releases/releases/download/updates/Flowstate-#{version}.dmg"
   name "Flowstate"
